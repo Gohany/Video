76ce7529f57f545d0891de99bb9c7a72bfe5c10c
Gohany <Gohany@gmail.com>
2015-09-21 11:53:14 -0400

    network discovery

--- test/mcast_recv.php
+++ test/mcast_recv.php
@@ -3,12 +3,17 @@
 error_reporting(E_ALL | E_STRICT);
 
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP); 
-socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, 1);
+socket_set_option($socket, SOL_SOCKET, SO_BROADCAST, 1);
+socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>1,"usec"=>0));
+
+$time = time();
+//$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP); 
+
+//socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, 1);
 
 function sendHello($sock, $port)
 {
-        $broadcast_string = 'hello world!';
+        $broadcast_string = 'hello world!3';
         print "SENDING TO PORT: " . $port . PHP_EOL;
         socket_sendto($sock, $broadcast_string, strlen($broadcast_string), 0, '255.255.255.255', $port);
 }
@@ -21,26 +26,30 @@ function sendHello($sock, $port)
 
 //socket_set_option($socket, SOL_SOCKET);//, MCAST_JOIN_SOURCE_GROUP, $mcast_group);
 $binded = socket_bind($socket, '0.0.0.0', 4444);
+//$binded = socket_bind($socket, '127.0.0.1', 4444);
 
 $from = '';
 $port = 4444;
 
-sendHello($sock, $port);
+sendHello($socket, $port);
 
 while (true)
 {
-        $port = 4444;
+        //$port = 4444;
         print "RECEIVING.. " . PHP_EOL;
-        socket_recvfrom($socket, $buf, 12, MSG_WAITALL, $from, $port);
+        print time() - $time . PHP_EOL;
+        socket_recvfrom($socket, $buf, 13, MSG_WAITALL, $from, $port);
         echo "Received $buf from remote address $from and remote port $port" . PHP_EOL;
-        sendHello($sock, $port);
-        sleep(1);
+        sendHello($socket, $port);
+        //sleep(1);
         
-        if (($errorcode = socket_last_error($socket)) || ($errorcode = socket_last_error($sock)))
+        if (($errorcode = socket_last_error($socket)) || ($errorcode = socket_last_error($socket)))
         {
                 $errormsg = socket_strerror($errorcode);
-                print "ERR0R: " . $errorcode . PHP_EOL;
+                print "ERR0R: " . $errormsg . PHP_EOL;
         }
+        
+        
         
 }
 
